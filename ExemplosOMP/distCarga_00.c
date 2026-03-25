9624ad6eb6c1fc9a69e7f9518c97579c9ccf24a0
etvorellana <evalero@uesc.br>
2026-03-25 09:42:08 -0300

    correções ExemplosOMP

--- ExemplosOMP/distCarga_00.c
+++ ExemplosOMP/distCarga_00.c
@@ -16,7 +16,8 @@ int main(void)
     double **med, *medAvg;
     unsigned int *qmed;
     double start, stop;
-    double max, min;
+    double max, min, avgTotal;
+    unsigned int qmedTotal;
 
     med = (double**) malloc(N*sizeof(double*));
     qmed = (unsigned int*)malloc(N*sizeof(unsigned int));
@@ -42,22 +43,33 @@ int main(void)
     start = omp_get_wtime();
     max = 0;
     min = 35;
-    for(int i = 0; i < N; i++){
-        double avg = 0;
-        for(int j = 0; j < qmed[i]; j++)
-        {
-            if ((j > 0) && (j < (qmed[i] - 1)) && (i > 0) && (i < (N-1)))
-                med[i][j] = (med[i][j-1] + med[i][j] + med[i][j+1] + med[i-1][j] + med[i+1][j])/5;
-            avg += med[i][j]/35;
-            if(max < med[i][j])
+    for (int k = 0; k < 1000; k++) {
+        avgTotal = 0;
+        qmedTotal = 0;
+        for(int i = 0; i < N; i++){
+            double avg = 0;
+            for(int j = 0; j < qmed[i]; j++)
             {
-                max = med[i][j];
-            }else if(min < med[i][j]){
-                min = med[i][j];
+                if ((j > 0) && (j < (qmed[i] - 1)))
+                    med[i][j] = (2*med[i][j-1] + 4*med[i][j] + 2*med[i][j+1])/8;
+                else if(j == 0)
+                    med[i][j] = (4*med[i][j] + 2*med[i][j+1])/6;
+                else
+                    med[i][j] = (2*med[i][j-1] + 4*med[i][j])/6;
+                avg += med[i][j]/35;
+                if(max < med[i][j])
+                {
+                    max = med[i][j];
+                }else if(min < med[i][j]){
+                    min = med[i][j];
+                }
             }
+            medAvg[i] = avg/qmed[i];
+            avgTotal += avg;
+            qmedTotal += qmed[i];
         }
-        medAvg[i] = avg/qmed[i];
     }
+    avgTotal = avgTotal/qmedTotal;
     stop = omp_get_wtime();
     printf("Tempo de processamento: %f\n", stop - start);
 
@@ -67,6 +79,8 @@ int main(void)
     printf("medAvg[%d] = %f\n", N-1,medAvg[N-1]);
     printf("Valor máximo: %f\n", max);
     printf("Valor mínimo: %f\n", min);
+    printf("Quantidade total de elementos: %u\n", qmedTotal);
+    printf("Média total: %f\n", avgTotal);
     stop = omp_get_wtime();
     printf("Tempo de saída: %f\n", stop - start);
     free(medAvg);
